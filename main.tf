b610829ed1ee8e0a6ae228e66e8affae14a49933
Paulo Cardoso <ph_cardoso@hotmail.com>
2023-12-11 10:59:34 -0300

    2

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name          = "cardosop-github-demo-bucket"
+  project       = "gcp-devops-402516"
   location      = "US"
   force_destroy = true
 
